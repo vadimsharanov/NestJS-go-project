a2cc7d422c5c3b886b3a7b4a7b622bc2d4ad24e7
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-11-11 19:36:15 +0200

    database connection

---
+++ src/tag/tag.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { TagService } from './tag.service';
+
+@Controller('tags')
+export class TagController {
+  constructor(private readonly tagService: TagService) {}
+  @Get()
+  findAll() {
+    return this.tagService.findAll();
+  }
+}

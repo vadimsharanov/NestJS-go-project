95f79095f6a499afde70ffb9fe40fd2aa5aa1a5c
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-11-16 22:29:40 +0200

    find by slug method added

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,16 +1,27 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from "@nestjs/common";
 import { User } from "src/user/decorators/user.decorator";
 import { UserEntity } from "src/user/entity/user.entity";
 import { AuthGuard } from "src/user/guard/auth.guard";
+import { ArticleEntity } from "./article.entity";
 import { ArticleService } from "./article.service";
 import { CreateArticleDto } from "./dto/createArticle.dto";
+import { ArticleResponseInterface } from "./types/article.response.interface";
 
 @Controller("articles")
 export class ArticleController {
 	constructor(private readonly articleService: ArticleService) {}
 	@Post()
 	@UseGuards(AuthGuard)
-	async create(@User() currentUser: UserEntity, @Body("article") createArticleDto: CreateArticleDto): Promise<any> {
-		return this.articleService.createArticle(currentUser, createArticleDto);
+	async create(
+		@User() currentUser: UserEntity,
+		@Body("article") createArticleDto: CreateArticleDto,
+	): Promise<ArticleResponseInterface> {
+		const article = await this.articleService.createArticle(currentUser, createArticleDto);
+		return this.articleService.buildArticleResponse(article);
+	}
+	@Get(":slug")
+	async getArticle(@Param("slug") slug: string): Promise<ArticleResponseInterface> {
+		const article = await this.articleService.findBySlug(slug);
+		return this.articleService.buildArticleResponse(article);
 	}
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -4,7 +4,8 @@ import { UserEntity } from "src/user/entity/user.entity";
 import { Repository } from "typeorm";
 import { ArticleEntity } from "./article.entity";
 import { CreateArticleDto } from "./dto/createArticle.dto";
-
+import { ArticleResponseInterface } from "./types/article.response.interface";
+import slugify from "slugify";
 @Injectable()
 export class ArticleService {
 	constructor(@InjectRepository(ArticleEntity) private readonly articleRepository: Repository<ArticleEntity>) {}
@@ -15,10 +16,23 @@ export class ArticleService {
 		if (!article.tagList) {
 			article.tagList = [];
 		}
-		article.slug = "foo";
+		article.slug = this.getSlug(createArticleDto.title);
+		console.log(article.slug);
 
 		article.author = currentUser;
 
 		return await this.articleRepository.save(article);
+	}
+
+	async findBySlug(slug: string): Promise<ArticleEntity> {
+		return await this.articleRepository.findOne({ slug });
+	}
+
+	buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+		return { article };
+	}
+
+	private getSlug(title: string): string {
+		return slugify(title, { lower: true }) + "-" + ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
 	}
 }

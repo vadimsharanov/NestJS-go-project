2f6e90a2aab985b801379265b30acc8abbe3563e
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-11-16 17:09:34 +0200

    small fixes added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-require('module-alias/register');
+require("module-alias/register");
 
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(3003);
 }
 bootstrap();
